545fe61d829237dc23f50243d7dd498a11b23511
nhyira007 <nhyiraboateng2019@gmail.com>
2022-12-08 20:44:49 -0800

    puts string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ * main is where the program is executed
+ * return always 0
+ */
+int main(void)
+{
+		puts("Programming is like building a multilingual puzzle\n");
+			return (0);
+}
