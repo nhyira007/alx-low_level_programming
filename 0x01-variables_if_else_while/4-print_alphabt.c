a1b9887180f76c4900e23f933fc58b15ed115b49
Nhyira007 <nhyiraboateng2019@gmail.com>
2022-12-15 02:50:53 +0000

    Print lower and uppercase alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print alphabet except q and e'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+		putchar('\n');
+		return (0);
+}
