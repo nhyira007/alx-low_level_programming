7ba0ec4f2c773c8e4be9b69901ffffb7bdd69cd4
Nhyira007 <nhyiraboateng2019@gmail.com>
2022-12-15 02:23:26 +0000

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'positive or negative'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+	return (0);
+}
