3380cb2ee820896572ab8e6e079e5ad38537318c
Nhyira007 <nhyiraboateng2019@gmail.com>
2022-12-15 03:01:09 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'prints numbers'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+		return (0);
+}
+
