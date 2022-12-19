22b51535fdf9066490a1f36e75f4a298a4c47469
Nhyira007 <nhyiraboateng2019@gmail.com>
2022-12-19 10:24:47 +0000

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	int i;
+        int i;
 
-	for (i = 0; i <= 9; i++)
-	{
-		putchar((i % 10) + '0');
+        for (i = 0; i <= 9; i++)
+        {
+                putchar((i % 10) + '0');
 
-		if (i == 9)
-			continue;
-			putchar(',');
-			putchar(' ');
-	}
-			putchar('\n');
-			return (0);
+                if (i == 9)
+                        continue;
+                        putchar(',');
+                        putchar(' ');
+        }
+                        putchar('\n');
+                        return (0);
 }
