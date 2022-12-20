a944d4effbce6c3d923604be7a3c2ec46180a20f
Nhyira007 <nhyiraboateng2019@gmail.com>
2022-12-20 16:51:08 +0000

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: 'alphabet x10'
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int ch;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
